0818e3281c06fa5b7d08881c152c1656ab7b8d11
uranuscraft <uranuscraft@gmail.com>
2014-05-14 20:39:25 -0400

    8:40
    added equation class
    added storage handle
          - working on if statements

--- java/obesityFactor/ObesityFactor.java
+++ java/obesityFactor/ObesityFactor.java
@@ -4,7 +4,11 @@ import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-
+/**
+ * 
+ * @author UranusCraft
+ *
+ */
 @Mod(modid = ObesityFactor.MODID, version = ObesityFactor.VERSION)
 public class ObesityFactor
 {
